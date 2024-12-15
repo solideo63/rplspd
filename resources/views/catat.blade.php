8ea908f1f88e43d0b91236fa25a333e17d37a0ad
Janitra Hayu <222212678@stis.ac.id>
2024-12-15 11:45:17 +0700

    merapikan label nim dan name di catat rutin

--- resources/views/catat.blade.php
+++ resources/views/catat.blade.php
@@ -7,20 +7,23 @@
             <h3 class="text-2xl font-bold dark:text-white">Pencatatan Pelanggaran Operasi Rutin Tingkat 1</h3>
             <p class="text-gray-800 dark:text-gray-400 italic opacity-50">(Mohon isi data pelanggaran di bawah ini)
             </p>
-           <form class="max-w-sm mx-auto pt-6" action="{{ route('operasi-rutin.store') }}" method="POST">
+            <form class="max-w-sm mx-auto pt-6" action="{{ route('operasi-rutin.store') }}" method="POST">
                 @csrf
                 <div class="mb-5">
-                    <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">nim</label>
+                    <label for="nim"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
                     <input type="text" id="nim" name="nim" required
                         class="shadow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div class="mb-5">
-                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
+                    <label for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
                     <input type="text" id="name" name="name" required
                         class="shadow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div class="mb-5">
-                    <label for="violation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Pelanggaran</label>
+                    <label for="violation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis
+                        Pelanggaran</label>
                     <input type="text" id="violation" name="violation" required
                         class="shadow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
@@ -31,9 +34,9 @@
                     </button>
                 </div>
             </form>
-            </div>
         </div>
     </div>
+</div>
 </div>
 
 <x-footer></x-footer>
