036ddc242ca908da6afc9090d61009c985e7e3c0
yupaa <auliazulfafa@gmail.com>
2024-12-17 11:20:15 +0700

    ubah tampilan perdir -dandi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,7 +102,7 @@
         <div class="flex justify-center">
             <h3 class="text-3xl font-bold dark:text-white pl-4 items-center">Peraturan Terkait</h3>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-4 p-4">
+        <div class="flex justify-center items-center w-full p-4">
             <div
                 class="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 md:p-6">
                 <a href="#">
@@ -117,65 +117,14 @@
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Tentang Kode Etik dan Tata
                         Tertib
                         Mahasiswa Politeknik Statistika STIS</p>
-                    <a href="#"
+                    <a href="https://drive.google.com/file/d/187H0ji_Rn0sYYO2yfx_NgQTN5Yetb14I/view?usp=sharing"
+                        target="_blank"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Baca Lebih Lanjut
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-
-            <div
-                class="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 md:p-6">
-                <a href="#">
-                    <img class="rounded-t-lg" src="img/perdir003.png" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Peraturan
-                            Direktur Politeknik Statistika Nomor 003 Tahun </h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Tentang Kode Etik dan Tata
-                        Tertib
-                        Mahasiswa Politeknik Statistika STIS</p>
-                    <a href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Baca Lebih Lanjut
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg>
-                    </a>
-                </div>
-            </div>
-
-            <div
-                class="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 md:p-6">
-                <a href="#">
-                    <img class="rounded-t-lg" src="img/perdir003.png" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Peraturan
-                            Direktur Politeknik Statistika Nomor 003 Tahun </h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Tentang Kode Etik dan Tata
-                        Tertib
-                        Mahasiswa Politeknik Statistika STIS</p>
-                    <a href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Baca Lebih Lanjut
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </a>
                 </div>
