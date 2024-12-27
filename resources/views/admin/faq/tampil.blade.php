f313e29caa47cf7d5b232e6a6f6da37a6e43fc10
yupaa <auliazulfafa@gmail.com>
2024-12-27 09:07:28 +0700

    tambahkan tampilan manage faq untuk admin

---
+++ resources/views/admin/faq/tampil.blade.php
@@ -0,0 +1,42 @@
+<x-layout></x-layout>
+
+<div class="p-4 sm:ml-64 mt-4 ">
+    <a href="{{  route('admin.tambah.faq') }}"><button type="button" style="background-color: #FCBE11;" class="text-white hover:opacity-90 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Tambah FaQ</button></a>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" id="export-table" class="datatable-table">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">Tanya</th>
+            <th scope="col" class="px-6 py-3">Jawab</th>
+            <th scope="col" class="px-6 py-3">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse($data as $row)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="px-6 py-4">{{ $row->tanya }}</td>
+                <td class="px-6 py-4">{{ $row->jawab }}</td>
+                <td class="px-6 py-4 flex-row">
+                    <a href="{{ route('admin.edit.faq', $row->id) }}">
+                        <button type="submit" class="px-4 py-2 text-white bg-yellow-300 rounded-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 ease-in-out">
+                            Edit
+                        </button>
+                    </a>
+                    <form action="{{ route('admin.delete.faq', $row->id) }}" method="post">
+                        @csrf
+                        <button type="submit" class="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 transition duration-300 ease-in-out">
+                            Hapus
+                        </button>
+
+                    </form>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="6" class="px-6 py-4 text-center">No data available</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
+</div>
+
+<x-footer></x-footer>
